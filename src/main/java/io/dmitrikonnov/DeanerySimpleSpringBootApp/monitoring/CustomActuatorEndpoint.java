ef2deabdcea73e9113a06f7375c63b18e4f14afa
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-01-26 19:58:49 +0300

    prometheus set ready to run

---
+++ src/main/java/io/dmitrikonnov/DeanerySimpleSpringBootApp/monitoring/CustomActuatorEndpoint.java
@@ -0,0 +1,21 @@
+package io.dmitrikonnov.DeanerySimpleSpringBootApp.monitoring;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Endpoint(id = "custom")
+@Component
+public class CustomActuatorEndpoint {
+
+
+    @ReadOperation
+    public Object customEndpoint () {
+        Map<String, String> map = new HashMap<>();
+        map.put("Key", "Value");
+        return map;
+    }
+}
