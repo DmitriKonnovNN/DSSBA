3757054485674a77058e9e73288b02d9c3c3834d
Dmitri Konnov <lildkonnov.nnov@inbox.ru>
2022-05-31 18:04:06 +0400

    et

--- src/main/java/io/dmitrikonnov/DeanerySimpleSpringBootApp/einstufungstest/EinstufungsTestAntwortenPruefer.java
+++ src/main/java/io/dmitrikonnov/DeanerySimpleSpringBootApp/einstufungstest/EinstufungsTestAntwortenPruefer.java
@@ -15,10 +15,6 @@ public class EinstufungsTestAntwortenPruefer {
 
     private final ETErgebnisse ergebnisse;
 
-   /* private void countCorectAnswersForEachNiveau (ETAufgabenNiveau niveau,ETErgebnisse ergebnisse) {
-        if (niveau.equals())
-    }*/
-
     public ETErgebnisse checkBogen(EinstufungsTestAntwortBogenDto antwortBogen, EinstufungsTestAufgabenBogen cachedAufgabenBogen) {
         List<EinstufungsTestAufgabe> cachedAufgaben = cachedAufgabenBogen.getAufgabenListe();
         ergebnisse.setAufgabenBogenHash(cachedAufgabenBogen.getAufgabenBogenHash());
@@ -35,12 +31,11 @@ public class EinstufungsTestAntwortenPruefer {
                     ergebnisse.getIdZuRichtigkeitMap().put(antwId, correct);
                     if (correct){
                         ergebnisse.getRichtigeLoesungenNachNiveau().add(aufgabe.getAufgabenNiveau().toString());
-                         /*  countCorectAnswersForEachNiveau(aufgabe.getAufgabenNiveau(),ergebnisse);*/
                     }
                 }
             });
         });
 
-        return null;
+        return ergebnisse;
     }
 }
